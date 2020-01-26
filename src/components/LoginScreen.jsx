0d8d11a7ed95b7bd75764ce68f91dbae014335cb
Siddharth Gore <siddharth.gore@live.com>
2020-01-26 01:15:41 -0500

    eslint

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,16 @@
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
